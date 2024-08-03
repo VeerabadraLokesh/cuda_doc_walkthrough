15bee0c5a4ca12f9a6a723aa1603d20ccb5deabe
Lokesh Paturu <lokesh.pvl@gmail.com>
2024-08-03 09:35:43 -0400

    start

---
+++ commands.sh
@@ -0,0 +1,4 @@
+nvcc vector_add.cu -o vector_add.out
+
+nvprof ./vector_add.out
+
---
+++ vector_add.cu
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10000000
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out;
+    float *d_a, *d_b, *d_out;
+
+    a = (float*)malloc(sizeof(float) * N);
+    b = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Allocate device memory for a
+    gpuErrchk(cudaMalloc((void**)&d_a, sizeof(float) * N));
+    gpuErrchk(cudaMalloc((void**)&d_b, sizeof(float) * N));
+    gpuErrchk(cudaMalloc((void**)&d_out, sizeof(float) * N));
+
+    // Transfer data from host to device memory
+    gpuErrchk(cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice));
+
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
+    
+    gpuErrchk(cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost));
+
+    // Cleanup after kernel execution
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+    free(a);
+    free(b);
+    free(out);
+
+    return 0;
+}
+
